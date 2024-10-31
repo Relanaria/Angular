917e99ba4c4db12e987c3296b3ab898fbb837d0f
Relanaria <shadowword9@gmail.com>
2024-10-31 11:46:08 +0200

    Update index.js

--- 01.Typescript/Simple Data Class/index.js
+++ 01.Typescript/Simple Data Class/index.js
@@ -1,5 +1,6 @@
 var httpRequest = /** @class */ (function () {
     function httpRequest(method, uri, version, message) {
+        this.method = method;
         this.uri = uri;
         this.version = version;
         this.message = message;
