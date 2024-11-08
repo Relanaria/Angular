050aa2dd9829a42ae664a808e6d2d3a301cb5ed1
Relanaria <shadowword9@gmail.com>
2024-11-08 14:54:07 +0200

    Login And Register Page
    -Added a Login page with a form and styles but no functionality
    -Added a Register page with a form and styles but no functionality

--- 02.Angular Components/component-app/src/app/app.component.ts
+++ 02.Angular Components/component-app/src/app/app.component.ts
@@ -2,11 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { MainComponent } from './main/main.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { FooterComponent } from './footer/footer.component';
+import { RegisterPageComponent } from './register-page/register-page.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, MainComponent],
+  imports: [RouterOutlet, HeaderComponent, MainComponent, LoginPageComponent, FooterComponent, RegisterPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
