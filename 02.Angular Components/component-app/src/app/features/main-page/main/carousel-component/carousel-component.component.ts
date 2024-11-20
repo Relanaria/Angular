b7b409f130393151c5d684f3312e8e4daf8a4712
Dobromir Yordanov <shadowword9@gmail.com>
2024-11-20 10:16:59 +0200

    Main Page
    -Changed hero-section layout
    -Changed collections article layout

--- 02.Angular Components/component-app/src/app/features/main-page/main/carousel-component/carousel-component.component.ts
+++ 02.Angular Components/component-app/src/app/features/main-page/main/carousel-component/carousel-component.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-carousel-component',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './carousel-component.component.html',
   styleUrl: './carousel-component.component.css'
 })
