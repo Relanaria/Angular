699feaabaa69612d6fb26ef690b21889d7600078
Relanaria <shadowword9@gmail.com>
2024-12-02 23:13:10 +0200

    RouteGuards Fix

--- 02.Angular Project/ClothingStore/src/app/guards/user-guards.guard.ts
+++ 02.Angular Project/ClothingStore/src/app/guards/user-guards.guard.ts
@@ -2,22 +2,24 @@ import { CanActivateFn } from '@angular/router';
 import { inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { UserAuthService } from '../features/user/user-auth.service';
+import { ActivatedRouteSnapshot } from '@angular/router';
+import { RouterStateSnapshot } from '@angular/router';
 
-
-export const userGuardsGuard: CanActivateFn = (route, state) => {
+export const userGuardsGuard: CanActivateFn = (route: ActivatedRouteSnapshot, state: RouterStateSnapshot) => {
   console.log({ route, state });
 
   const userService = inject(UserAuthService);
   const router = inject(Router);
   // const isLoggedIn = true; 
 
-  if(!userService.user){
-    router.navigate(['/login']);
+  if(userService.user){
+    return true;
   }
-  
+
+  router.navigate(['/login']);
   // if (!isLoggedIn) {
   //   router.navigate(['/login']);
   // }
 
-  return isLoggedIn;
+  return false;
 };
