3134380221d81c7236e9fbc6fda64fecdf2424de
Relanaria <shadowword9@gmail.com>
2024-12-02 22:43:27 +0200

    AuthGuard
    -Guard now know if we have a logged in user or not se it can properly navigate out of page to login if there is no user logged

--- 02.Angular Project/ClothingStore/src/app/guards/user-guards.guard.ts
+++ 02.Angular Project/ClothingStore/src/app/guards/user-guards.guard.ts
@@ -1,12 +1,19 @@
 import { CanActivateFn } from '@angular/router';
 import { inject } from '@angular/core';
 import { Router } from '@angular/router';
+import { UserAuthService } from '../features/user/user-auth.service';
+
 
 export const userGuardsGuard: CanActivateFn = (route, state) => {
   console.log({ route, state });
 
+  const userService = inject(UserAuthService);
   const router = inject(Router);
   const isLoggedIn = true; // will come from AuthService
+
+  if(!userService.user){
+    router.navigate(['/login']);
+  }
   
   if (!isLoggedIn) {
     router.navigate(['/login']);
