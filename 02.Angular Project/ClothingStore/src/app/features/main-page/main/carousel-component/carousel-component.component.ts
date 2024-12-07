4ee2594a4b453a8f7f6b9a832c79e1a0e19cc1f6
Dobromir Yordanov <shadowword9@gmail.com>
2024-12-07 13:50:35 +0200

    Added scroll smooth

--- 02.Angular Project/ClothingStore/src/app/features/main-page/main/carousel-component/carousel-component.component.ts
+++ 02.Angular Project/ClothingStore/src/app/features/main-page/main/carousel-component/carousel-component.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { ViewportScroller } from '@angular/common';
 
 @Component({
   selector: 'app-carousel-component',
   standalone: true,
-  imports: [RouterLink],
+  imports: [],
   templateUrl: './carousel-component.component.html',
   styleUrl: './carousel-component.component.css'
 })
 export class CarouselComponentComponent {
 
+
+  constructor(private scroller: ViewportScroller){}
+
+  
+  scrollTo(){
+    this.scroller.scrollToAnchor("collections")
+  }
 }
